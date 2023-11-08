b2316c6609a39583cc16bf6eed5d3a0cc5b49215
Acle <99556572+Acle23@users.noreply.github.com>
2023-11-08 19:47:41 +0100

    version control

--- JS/index.js
+++ JS/index.js
@@ -5,3 +5,19 @@ underline.forEach((item) => {
         item.classList.add("active");
     });
 });
+
+const mainMenu = document.querySelector('.mainMenu');
+const closeMenu = document.querySelector('.closeMenu');
+const openMenu = document.querySelector('.openMenu');
+
+openMenu.addEventListener('click', show);
+closeMenu.addEventListener('click', close);
+
+function show() {
+    mainMenu.style.display = 'flex';
+    mainMenu.style.top = '0';
+}
+
+function close() {
+    mainMenu.style.top = '-100%';
+}
